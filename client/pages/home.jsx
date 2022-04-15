7c3e57ec6670d20edec36b3e2a2d597c64943cae
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-15 19:40:37 +0000

    Successfully added a react example of react-bootstrap to final project.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 export default function Home(props) {
   return (
     <>
-    <h1>Hello</h1>
+    <Container>
+      <Row className="justify-content-center mt-5">
+        <Form>
+          <Form.Group className="mb-2" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+         </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Row>
+    </Container>
     </>
   );
 }
