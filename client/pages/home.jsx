2ccbb3a696e1e458b6118cf8eaec6326e1c54e24
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-16 22:19:17 +0000

    WIP feature 1

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -6,9 +6,24 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default class Home extends React.Component {
-  // constructor(props) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomName: '',
+      username: '',
+      youtubeVideo: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  // handleChange(event) {
+  //   const { roomName, username, youtubeVideo } = event.target;
   // }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
@@ -18,15 +33,15 @@ export default class Home extends React.Component {
            <Row className="justify-content-center mt-5">
              <Col xs="12" lg="4">
                 <Form.Label className="font-label">Room Name</Form.Label>
-                <Form.Control className="mb-4" type="text" required />
+                <Form.Control className="mb-4" type="roomName" required />
               </Col>
               <Col xs="12" lg="4">
                 <Form.Label className="font-label">Username</Form.Label>
-                <Form.Control className="mb-4" type="text" required />
+                <Form.Control className="mb-4" type="username" required />
              </Col>
               <Col xs="12" lg="4">
                 <Form.Label className="font-label">Insert Youtube Video</Form.Label>
-                <Form.Control className="mb-4" type="text" required />
+                <Form.Control className="mb-4" type="youtubeVideo" required />
               </Col>
             </Row>
            <Button className="float-end mt-2" variant="primary" type="submit">
