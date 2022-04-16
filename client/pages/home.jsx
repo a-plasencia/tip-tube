d8276a790edb1a817413f3515d5787109ea06ac8
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-16 00:17:59 +0000

    Have a form component using react bootstrap.  Make sure the button type is submit, verifies that the user is not submitting any fields.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -3,34 +3,38 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
 
-export default function Home(props) {
-  return (
-    <>
-    <Container>
-      <Row className="justify-content-center mt-5">
-        <Form>
-          <Form.Group className="mb-2" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            </Form.Text>
-          </Form.Group>
+export default class Home extends React.Component {
+  // constructor(props) {
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-         </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Row>
-    </Container>
-    </>
-  );
+  // }
+
+  render() {
+    return (
+      <>
+        <Container>
+          <Form>
+           <Row className="justify-content-center mt-5">
+             <Col xs="12" lg="4">
+                <Form.Label className="font-label">Room Name</Form.Label>
+                <Form.Control className="mb-4" type="text" required />
+              </Col>
+              <Col xs="12" lg="4">
+                <Form.Label className="font-label">Username</Form.Label>
+                <Form.Control className="mb-4" type="text" required />
+             </Col>
+              <Col xs="12" lg="4">
+                <Form.Label className="font-label">Insert Youtube Video</Form.Label>
+                <Form.Control className="mb-4" type="text" required />
+              </Col>
+            </Row>
+           <Button className="float-end mt-2" variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </>
+    );
+  }
 }
