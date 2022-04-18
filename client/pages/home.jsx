ec9c96c19aa3dcdc0b14e54a8191d5589fcf76a0
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-18 01:42:08 +0000

    Finished feature 1 functionality do cleanup code into pull request.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -13,35 +13,50 @@ export default class Home extends React.Component {
       username: '',
       youtubeVideo: ''
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleChange(event) {
-  //   const { roomName, username, youtubeVideo } = event.target;
-  // }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  }
 
   handleSubmit(event) {
     event.preventDefault();
+    const req = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch('/api/create', req)
+      .then(res => res.json())
+      .then(result => {
+        // console.log(result);
+      });
   }
 
   render() {
     return (
       <>
         <Container>
-          <Form>
+          <Form onSubmit={this.handleSubmit}>
            <Row className="justify-content-center mt-5">
              <Col xs="12" lg="4">
                 <Form.Label className="font-label">Room Name</Form.Label>
-                <Form.Control className="mb-4" type="roomName" required />
+                <Form.Control onChange={this.handleInputChange} value={this.state.roomName} name="roomName" className="mb-4" type="text" required />
               </Col>
               <Col xs="12" lg="4">
                 <Form.Label className="font-label">Username</Form.Label>
-                <Form.Control className="mb-4" type="username" required />
+                <Form.Control onChange={this.handleInputChange} value={this.state.username} name="username" className="mb-4" type="text" required />
              </Col>
               <Col xs="12" lg="4">
                 <Form.Label className="font-label">Insert Youtube Video</Form.Label>
-                <Form.Control className="mb-4" type="youtubeVideo" required />
+                <Form.Control onChange={this.handleInputChange} value={this.state.youtubeVideo} name="youtubeVideo" className="mb-4" type="text" required />
               </Col>
             </Row>
            <Button className="float-end mt-2" variant="primary" type="submit">
--- server/index.js
+++ server/index.js
@@ -57,3 +57,4 @@ app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
 });
+// Adding comment
