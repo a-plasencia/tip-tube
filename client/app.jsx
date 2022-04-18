6ef4d3980413a5e305dfbef06eabeea82ce27e7a
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-18 20:12:58 +0000

    Added a route state with the window.location.hash in app.jsx.

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import Home from './pages/home';
 import NavbarScreen from './pages/navbar';
-// import parseRoute from '../server/parse-route';
+import parseRoute from '../server/parse-route';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
+  }
+
   render() {
     return (
       <>
