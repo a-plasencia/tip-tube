b01e5565c88507cd45558aac16c97406cc319032
Alex Plasencia <aPlasencia895@hotmail.com>
2022-04-18 20:04:21 +0000

    Make a parse-route.js and create a file with a parseRoute function that takes a hashroute as an argument.

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/home';
 import NavbarScreen from './pages/navbar';
+// import parseRoute from '../server/parse-route';
 
 export default class App extends React.Component {
   render() {
