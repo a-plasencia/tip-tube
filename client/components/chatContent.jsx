14b7b218275d4aed313c1a116b393ea0d85ea225
Alex Plasencia <aPlasencia895@hotmail.com>
2022-06-18 20:44:16 +0000

    In chatContext.jsx add a regex const variable that looks for timestamps in 00:00:00 format.

--- client/components/chatContent.jsx
+++ client/components/chatContent.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 export default class ChatContent extends React.Component {
 
   render() {
+    // eslint-disable-next-line no-unused-vars
+    const tsRegex = /\d{2}:\d{2}:\d{2}/g;
     return (
     <ul>
       {
