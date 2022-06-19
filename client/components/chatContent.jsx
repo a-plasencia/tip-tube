e55698d67d4d273bc5e04afb28802c19599a1cab
Alex Plasencia <aPlasencia895@hotmail.com>
2022-06-19 07:03:17 +0000

    Added a class and attribute to all the spans and made anything with a data-ts attribute clickable.

--- client/components/chatContent.jsx
+++ client/components/chatContent.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import DomPurify from 'dompurify';
 
 export default class ChatContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onTsClick = this.onTsClick.bind(this);
+  }
+
+  onTsClick(event) {
+    const timeStamp = event.target.getAttribute('data-ts');
+    // eslint-disable-next-line no-console
+    console.log('value of timestamp is: ', timeStamp);
+  }
 
   render() {
-    // eslint-disable-next-line no-unused-vars
-    const tsRegex = /\d{2}:\d{2}:\d{2}/g;
     return (
     <ul>
       {
-      this.props.messages.map(message => (
-        <li key={message.messageId}>{message.username}: {message.content}</li>
-      ))
-  }
+        this.props.messages.map(message => (
+          <Message onClick={this.onTsClick} key={message.messageId} message={message} />
+        ))
+      }
     </ul>
     );
   }
+}
+
+const tsRegex = /\d{2}:\d{2}:\d{2}/g;
+
+function Message(props) {
+  const content = props.message.content.replaceAll(tsRegex, ts => {
+    return `<span class="timestamp" data-ts="${ts}">${ts}</span>`;
+  });
+  return (
+    <li dangerouslySetInnerHTML={{ __html: DomPurify.sanitize(content) }}></li>
+  );
 }
