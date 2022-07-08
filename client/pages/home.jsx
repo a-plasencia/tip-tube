6a50e5b494ed6549a78774df7a76d802fbf24085
Alex Plasencia <aPlasencia895@hotmail.com>
2022-07-08 00:43:39 +0000

    Home page now has a proper fetch error.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -5,13 +5,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       roomName: '',
-      youtubeVideo: ''
+      youtubeVideo: '',
+      loading: false,
+      submit: true,
+      error: false
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,8 +28,36 @@ export default class Home extends React.Component {
     this.setState({ [name]: value });
   }
 
+  loadingSpinner() {
+    if (this.state.loading === true) {
+      return '';
+    } else {
+      return 'd-none';
+    }
+  }
+
+  submitSpinner() {
+    if (this.state.submit === true) {
+      return 'Submit';
+    } else {
+      return '';
+    }
+  }
+
+  error() {
+    if (this.state.error === true) {
+      return 'justify-content-center mt-4';
+    } else {
+      return 'd-none justify-content-center mt-4';
+    }
+  }
+
   handleSubmit(event) {
     event.preventDefault();
+    this.setState({
+      loading: true,
+      submit: false
+    });
     const req = {
       method: 'POST',
       headers: {
@@ -37,15 +69,24 @@ export default class Home extends React.Component {
       .then(res => res.json())
       .then(result => {
         window.location.hash = `room?roomId=${result.roomId}`;
+      })
+      .catch(err => {
+        this.setState({
+          error: true
+        });
+        console.error('Fetch failed', err);
       });
   }
 
   render() {
+    const loadingSpinner = this.loadingSpinner();
+    const submit = this.submitSpinner();
+    const error = this.error();
     return (
       <>
         <Container>
           <Form onSubmit={this.handleSubmit}>
-           <Row className="justify-content-center mt-5">
+            <Row className="justify-content-center mt-5">
              <Col xs="12" lg="6">
                 <Form.Label className="font-label">Room Name</Form.Label>
                 <Form.Control onChange={this.handleInputChange} value={this.state.roomName} name="roomName" className="mb-4" type="text" required />
@@ -55,17 +96,24 @@ export default class Home extends React.Component {
                 <Form.Control onChange={this.handleInputChange} value={this.state.youtubeVideo} name="youtubeVideo" className="mb-4" type="text" required />
               </Col>
             </Row>
-           <Button className="float-end mt-2" variant="primary" type="submit">
-             <Spinner
-             className="hidden"
-             as="span"
-             animation="border"
-             role="status"
-             size="sm"
-             aria-hidden="true"
-             />
-              Submit
-            </Button>
+            <Row>
+              <Col>
+                <Button className="float-end mt-2 mb-2" variant="primary" type="submit">
+                {submit}
+                <Spinner
+                className={`${loadingSpinner}`}
+                as="span"
+                animation="border"
+                role="status"
+                size="sm"
+                aria-hidden="true"
+                />
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Alert variant="primary" className={`${error}`}>Sorry, there was an error connecting to the network! Please check your internet connection and try again.</Alert>
+            </Row>
           </Form>
         </Container>
       </>
